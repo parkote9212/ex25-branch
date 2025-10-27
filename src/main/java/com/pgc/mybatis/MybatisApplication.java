252352ec6a282dc5fb3701ca3b3468f462f31671
parkote9212 <parkote9212@gmail.com>
2025-10-27 15:14:50 +0900

    add DBConn

--- src/main/java/com/pgc/mybatis/MybatisApplication.java
+++ src/main/java/com/pgc/mybatis/MybatisApplication.java
@@ -1,10 +1,8 @@
 package com.pgc.mybatis;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
 @MapperScan("com.pgc.mybatis.mapper")
