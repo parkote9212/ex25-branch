211ccd877c519ed3256d4ecb75d7b203f1778bda
parkote9212 <parkote9212@gmail.com>
2025-10-28 10:34:08 +0900

    db conn update

--- src/main/java/com/pgc/mybatis/controller/DBConnTest.java
+++ src/main/java/com/pgc/mybatis/controller/DBConnTest.java
@@ -1,26 +1,29 @@
 package com.pgc.mybatis.controller;
 
-import java.sql.Connection;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @RestController
+@RequiredArgsConstructor
 public class DBConnTest {
 
-    @Autowired
-    private DataSource dataSource;
+    private static final Logger log = LoggerFactory.getLogger(DBConnTest.class);
+    private final DataSource dataSource;
 
     @GetMapping("/dbconn")
     public String dbconn() {
         try (Connection conn = dataSource.getConnection()) {
             String result = conn.getMetaData().getURL();
+            log.info("DB 연결 성공: {}" + result); // 로그 출력
             return "DB연결 성공: " + result;
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error("DB 연결 실패" + e); // 에러 로그 출력
             return "DB연결실패";
         }
     }
